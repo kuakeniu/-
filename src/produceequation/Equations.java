f1bb1f5aeaf7561a4c24f7587de2c33ae5970a65
kuakeniu <2386537498@qq.com>
2020-12-01 21:20:43 +0800

    fix: v1.0

--- src/produceequation/Equations.java
+++ src/produceequation/Equations.java
@@ -6,7 +6,7 @@ public class Equations {
     public static Formula getRandom(int addPercentage){
         int a, b, op,isAddorSub;
         isAddorSub=(int)(Math.random()*(101));
-        if(isAddorSub<=addPercentage){                   //如果小于百分比则为加法
+        if(isAddorSub<=addPercentage){                     //如果小于百分比则为加法
             a=(int)(Math.random()*101);
             b=(int)(Math.random()*(101-a));
             op=1;
